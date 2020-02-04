f30245f5e6d6bdb3f7e3383d48748591a8533027
TomNPython <tom.nicholson1000@gmail.com>
2020-02-04 14:55:37 +0700

    hard data added

--- scripts.js
+++ scripts.js
@@ -1,18 +1,79 @@
 var data = [
-    {x: "October", y: "Wk1", heat: 15},
-    {x: "October", y: "Wk2", heat: 17},
-    {x: "October", y: "Wk3", heat: 21},
-    {x: "October", y: "Wk4", heat: 34},
-    {x: "November", y: 'Wk1', heat: 33},
-    {x: "November", y: "Wk2", heat: 32},
-    {x: "November", y: "Wk3", heat: 51},
-    {x: "November", y: "Wk4", heat: 50},
-    {x: "December", y: "Wk1", heat: 47}
+    {x: "October Wk 1", y: "Mon", heat: 1000},
+    {x: "October Wk 1", y: "Tues", heat: 1100},
+    {x: "October Wk 1", y: "Weds", heat: 800},
+    {x: "October Wk 1", y: "Thurs", heat: 1000},
+    {x: "October Wk 1", y: "Fri", heat: 600},
+    {x: "October Wk 2", y: "Mon", heat: 400},
+    {x: "October Wk 2", y: "Tues", heat: 1000},
+    {x: "October Wk 2", y: "Weds", heat: 1000},
+    {x: "October Wk 2", y: "Thurs", heat: 1100},
+    {x: "October Wk 2", y: "Fri", heat: 1000},
+    {x: "October Wk 3", y: "Mon", heat: 500},
+    {x: "October Wk 3", y: "Tues", heat: 1300},
+    {x: "October Wk 3", y: "Weds", heat: 1100},
+    {x: "October Wk 3", y: "Thurs", heat: 4000},
+    {x: "October Wk 3", y: "Fri", heat: 1000},
+    {x: "October Wk 4", y: "Mon", heat: 400},
+    {x: "October Wk 4", y: "Tues", heat: 500},
+    {x: "October Wk 4", y: "Weds", heat: 500},
+    {x: "October Wk 4", y: "Thurs", heat: 400},
+    {x: "October Wk 4", y: "Fri", heat: 1500},
+    {x: "November Wk 1", y: "Mon", heat: 900},
+    {x: "November Wk 1", y: "Tues", heat: 1500},
+    {x: "November Wk 1", y: "Weds", heat: 300},
+    {x: "November Wk 1", y: "Thurs", heat: 500},
+    {x: "November Wk 1", y: "Fri", heat: 0},
+    {x: "November Wk 2", y: "Mon", heat: 500},
+    {x: "November Wk 2", y: "Tues", heat: 1200},
+    {x: "November Wk 2", y: "Weds", heat: 1200},
+    {x: "November Wk 2", y: "Thurs", heat: 500},
+    {x: "November Wk 2", y: "Fri", heat: 300},
+    {x: "November Wk 3", y: "Mon", heat: 1500},
+    {x: "November Wk 3", y: "Tues", heat: 0},
+    {x: "November Wk 3", y: "Weds", heat: 0},
+    {x: "November Wk 3", y: "Thurs", heat: 500},
+    {x: "November Wk 3", y: "Fri", heat: 2400},
+    {x: "November Wk 4", y: "Mon", heat: 2600},
+    {x: "November Wk 4", y: "Tues", heat: 1700},
+    {x: "November Wk 4", y: "Weds", heat: 800},
+    {x: "November Wk 4", y: "Thurs", heat: 0},
+    {x: "November Wk 4", y: "Fri", heat: 400},
+    {x: "December Wk 1", y: "Mon", heat: 100},
+    {x: "December Wk 1", y: "Tues", heat: 100},
+    {x: "December Wk 1", y: "Weds", heat: 700},
+    {x: "December Wk 1", y: "Thurs", heat: 1600},
+    {x: "December Wk 1", y: "Fri", heat: 800},
+    {x: "December Wk 2", y: "Mon", heat: 250},
+    {x: "December Wk 2", y: "Tues", heat: 700},
+    {x: "December Wk 2", y: "Weds", heat: 450},
+    {x: "December Wk 2", y: "Thurs", heat: 200},
+    {x: "December Wk 2", y: "Fri", heat: 400},
+    {x: "December Wk 3", y: "Mon", heat: 900},
+    {x: "December Wk 3", y: "Tues", heat: 200},
+    {x: "December Wk 3", y: "Weds", heat: 900},
+    {x: "December Wk 3", y: "Thurs", heat: 850},
+    {x: "December Wk 3", y: "Fri", heat: 1600},
+    {x: "December Wk 4", y: "Mon", heat: 850},
+    {x: "December Wk 4", y: "Tues", heat: 500},
+    {x: "December Wk 4", y: "Weds", heat: 700},
+    {x: "December Wk 4", y: "Thurs", heat: 500},
+    {x: "December Wk 4", y: "Fri", heat: 500},
   ];
 
   
   // create a chart and set the data
-  chart = anychart.heatMap(data);
+  let chart = anychart.heatMap(data);
+
+  let colourScale = anychart.scales.ordinalColor();
+  colourScale.ranges([
+    {less: 400}, 
+    {from: 401, to: 799},
+    {greater: 800}
+  ])
+  colourScale.colors(['red', 'yellow', 'green'])
+
+  chart.colorScale(colourScale)
   
   // set the container id
   chart.container("container");
